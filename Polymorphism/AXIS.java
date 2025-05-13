77618ca640d27a0ae177a102f4cb6e9ee8517315
FidelSR <113220737+FidelSR@users.noreply.github.com>
2025-05-13 16:40:37 -0400

    poly example

---
+++ Polymorphism/AXIS.java
@@ -0,0 +1,5 @@
+public class AXIS extends Bank {
+    public float getRateOfInterest(){
+        return 9;
+    }
+}
--- Polymorphism/BankExample.java
+++ Polymorphism/BankExample.java
@@ -1,5 +1,11 @@
 public class BankExample{
     public static void main(String[] args){
-        System.out.println("hi");
+        AXIS axis = new AXIS();
+        ICICI ici = new ICICI();
+        SBI sbi = new SBI();
+        System.out.println("Axis has an interest rate of " + axis.getRateOfInterest() + " %");
+        System.out.println("ICICI has an interest rate of " + ici.getRateOfInterest() + " %");
+        System.out.println("SBI has an interest rate of " + sbi.getRateOfInterest() + " %");
+
     }
 }
