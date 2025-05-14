1957f76346304602fb74f8987374148eb790e368
FidelSR <113220737+FidelSR@users.noreply.github.com>
2025-05-13 23:11:30 -0400

    Modified Polymorphism Example to better reflect the core concept

--- Polymorphism/BankExample.java
+++ Polymorphism/BankExample.java
@@ -1,8 +1,8 @@
 public class BankExample{
     public static void main(String[] args){
-        AXIS axis = new AXIS();
-        ICICI ici = new ICICI();
-        SBI sbi = new SBI();
+        Bank axis = new AXIS();
+        Bank ici = new ICICI();
+        Bank sbi = new SBI();
         System.out.println("Axis has an interest rate of " + axis.getRateOfInterest() + " %");
         System.out.println("ICICI has an interest rate of " + ici.getRateOfInterest() + " %");
         System.out.println("SBI has an interest rate of " + sbi.getRateOfInterest() + " %");
